173488cea099cc6622943fd59e760e291cbbe676
Alba <albaaparicio12@gmail.com>
2021-02-21 17:48:09 +0100

    SDI-2021-102-session2-ejercicio-complementario4

--- sdi2021-101-spring/src/main/java/com/uniovi/controllers/ProfessorsController.java
+++ sdi2021-101-spring/src/main/java/com/uniovi/controllers/ProfessorsController.java
@@ -3,11 +3,7 @@ package com.uniovi.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.*;
 import com.uniovi.entities.Professor;
 import com.uniovi.services.ProfessorService;
 
@@ -45,6 +41,19 @@ public class ProfessorsController {
 	@RequestMapping(value="/professor/add")
 	public String getProfessor(){
 		return "professor/add";
+	}
+	
+	@RequestMapping(value="/professor/edit/{id}")
+	public String getEdit(Model model, @PathVariable Long id){
+		model.addAttribute("professor", professorService.getProfessor(id));
+		return "professor/edit";
+	}
+	
+	@RequestMapping(value="/professor/edit/{id}", method=RequestMethod.POST)
+	public String setEdit(Model model, @PathVariable Long id, @ModelAttribute Professor professor){
+		professor.setId(id);
+		professorService.addProfessor(professor);
+		return "redirect:/professor/details/"+id;
 	}
 
 }
